2b549a754a2805c9f4a6872966abc737c88f0100
lucascoquillat4 <166069244+lucascoquillat4@users.noreply.github.com>
2025-01-20 11:17:34 +0100

    push achats

--- js/app.js
+++ js/app.js
@@ -1,13 +1,35 @@
-//variables
-let first_name = "Lucas";
-var age = 18;
-
-//cond
-if (10 == 10){
-    console.log("Bienvenue!")}
-    else{
-    console.log("Tu n'as pas l'âge requis.")
+// personnage
+let personnage = {
+    nom: "Lucas",
+    argent: 100, //argent disponible
+  };
+  
+//boutique
+  let boutique = {
+    produit: "Chocolatine",
+    quantite: 10, //quantité initiale
+    prix: 8, //prix unitaire
+  };
+  
+  //achats
+  function acheterProduit(personnage, boutique) {
+    let produitsAchetes = 0; 
+  
+    //achats à effectuer
+    while (personnage.argent >= boutique.prix && boutique.quantite > 0) {
+      personnage.argent -= boutique.prix; 
+      boutique.quantite--; //dim nbre de produits en boutique
+      produitsAchetes++; // augm nbre de produits achetés
     }
-
-console.log(12/6);
-
+  
+    //affichage console
+    console.log(`${personnage.nom} a acheté ${produitsAchetes} ${boutique.produit}(s).`);
+    console.log(`Il reste ${personnage.argent}€ à ${personnage.nom}.`);
+    console.log(
+      `Le produit est ${boutique.quantite === 0 ? "en rupture de stock" : "encore disponible"}.`
+    );
+  }
+  
+  // Appel de la fonction pour effectuer les achats
+  acheterProduit(personnage, boutique);
+  
