04cffbdcac010c6388e4d6d83f1217cf6b7ab572
lucascoquillat4 <166069244+lucascoquillat4@users.noreply.github.com>
2025-01-25 17:42:23 +0100

    tamere

---
+++ darkmode/darkmode/darkmode.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('toggle-dark-mode');
+button.addEventListener('click', () => {  document.body.classList.toggle('dark-mode');
+  button.textContent = document.body.classList.contains('dark-mode')
+    ? 'Désactiver le mode sombre'
+    : 'Activer le mode sombre';
+});
